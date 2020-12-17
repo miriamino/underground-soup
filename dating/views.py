9fe5d60e496f9e7cff0d205036450327ab1535c8
miriamino <29210881+miriamino@users.noreply.github.com>
2020-12-17 21:17:11 +0100

    bkla

--- dating/views.py
+++ dating/views.py
@@ -23,7 +23,7 @@ class IndexView(LoginRequiredMixin, generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:100]
 
 class QuestionSelfView(LoginRequiredMixin, generic.DetailView):
     model = Question
